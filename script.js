0664e6f0de901da2b6a688b5440b747fa70012e5
PutsGui <guiabc777@gmail.com>
2024-06-04 00:47:52 +0100

    Entrega Portfolio Guilherme Alves 3210202

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Smooth scrolling for anchor links
+    // Scroll suave ao clicar nos links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function(e) {
         e.preventDefault();
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   
-    // GSAP ScrollTrigger setup
+    // Mudança de cor de fundo e texto ao rolar a página
     gsap.registerPlugin(ScrollTrigger);
   
     const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
@@ -39,6 +39,80 @@ document.addEventListener('DOMContentLoaded', function() {
           })
       });
     });
-  });
   
+    // Animação do nome
+    const nameLink = document.getElementById('nameLink');
+    const fullName = 'LHERME';
+    let currentIndex = 0;
+    let intervalId;
+  
+    nameLink.addEventListener('mouseenter', () => {
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        if (currentIndex < fullName.length) {
+          nameLink.textContent += fullName[currentIndex];
+          currentIndex++;
+        } else {
+          clearInterval(intervalId);
+        }
+      }, 100); 
+    });
+  
+    nameLink.addEventListener('mouseleave', () => {
+      nameLink.textContent = 'GUI';
+      currentIndex = 0;
+      clearInterval(intervalId);
+    });
+  
+    // Cursor personalizado
+    const cursor = document.querySelector('.cursor');
+    const links = document.querySelectorAll('a');
+  
+    document.body.addEventListener('mousemove', onMouseMove);
+  
+    links.forEach(link => {
+      link.addEventListener('mouseenter', onMouseEnterLink);
+      link.addEventListener('mouseleave', onMouseLeaveLink);
+    });
+  
+    function onMouseMove(e) {
+      const rect = cursor.getBoundingClientRect();
+      const cursorX = e.clientX - rect.width / 2; 
+      const cursorY = e.clientY - rect.height / 2; 
+      
+      if (e.target.closest('section')) {
+        cursor.style.display = 'block'; 
+        cursor.style.top = cursorY + 'px';
+        cursor.style.left = cursorX + 'px';
+      } else {
+        cursor.style.display = 'none'; 
+      }
+    }
+  
+    function onMouseEnterLink() {
+      cursor.classList.add('hovered');
+    }
+  
+    function onMouseLeaveLink() {
+      cursor.classList.remove('hovered');
+    }
+  
+    // Efeito de imagem no projeto
+    const images = document.querySelectorAll('.image-list img');
+    const titles = document.querySelectorAll('.title-list h3');
+  
+    titles.forEach((title, index) => {
+      title.addEventListener('mouseenter', () => {
+        images.forEach(img => img.style.opacity = 0);
+  
+        const dataIndex = parseInt(title.getAttribute('data-index'));
+  
+        images[dataIndex].style.opacity = 1;
+      });
+  
+      title.addEventListener('mouseleave', () => {
+        images.forEach(img => img.style.opacity = 0);
+      });
+    });
+  });
   
