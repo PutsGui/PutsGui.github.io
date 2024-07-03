597f2a8159649f74877f0fa18628812d4e4ee47c
PutsGui <guiabc777@gmail.com>
2024-07-03 15:28:58 +0100

    update

--- script.js
+++ script.js
@@ -1,118 +1,145 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Scroll suave ao clicar nos links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  // Scroll suave ao clicar nos links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-          target.scrollIntoView({
-            behavior: 'smooth'
-          });
-        }
+          e.preventDefault();
+          const target = document.querySelector(this.getAttribute('href'));
+          if (target) {
+              target.scrollIntoView({
+                  behavior: 'smooth'
+              });
+          }
       });
-    });
-  
-    // Mudança de cor de fundo e texto ao rolar a página
-    gsap.registerPlugin(ScrollTrigger);
-  
-    const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
-  
-    scrollColorElems.forEach((colorSection, i) => {
+  });
+
+  // Mudança de cor de fundo e texto ao rolar a página
+  gsap.registerPlugin(ScrollTrigger);
+
+  const scrollColorElems = document.querySelectorAll("[data-bgcolor]");
+
+  scrollColorElems.forEach((colorSection, i) => {
       const prevBg = i === 0 ? "" : scrollColorElems[i - 1].dataset.bgcolor;
       const prevText = i === 0 ? "" : scrollColorElems[i - 1].dataset.textcolor;
-  
+
       ScrollTrigger.create({
-        trigger: colorSection,
-        start: "top 50%",
-        end: "bottom 50%",
-        onEnter: () =>
-          gsap.to("body", {
-            backgroundColor: colorSection.dataset.bgcolor,
-            color: colorSection.dataset.textcolor,
-            overwrite: "auto"
-          }),
-        onLeaveBack: () =>
-          gsap.to("body", {
-            backgroundColor: prevBg,
-            color: prevText,
-            overwrite: "auto"
-          })
+          trigger: colorSection,
+          start: "top 50%",
+          end: "bottom 50%",
+          onEnter: () =>
+              gsap.to("body", {
+                  backgroundColor: colorSection.dataset.bgcolor,
+                  color: colorSection.dataset.textcolor,
+                  overwrite: "auto"
+              }),
+          onLeaveBack: () =>
+              gsap.to("body", {
+                  backgroundColor: prevBg,
+                  color: prevText,
+                  overwrite: "auto"
+              })
       });
-    });
-  
-    // Animação do nome
-    const nameLink = document.getElementById('nameLink');
-    const fullName = 'LHERME';
-    let currentIndex = 0;
-    let intervalId;
-  
-    nameLink.addEventListener('mouseenter', () => {
+  });
+
+  // Animação do nome
+  const nameLink = document.getElementById('nameLink');
+  const fullName = 'LHERME';
+  let currentIndex = 0;
+  let intervalId;
+
+  nameLink.addEventListener('mouseenter', () => {
       clearInterval(intervalId);
       intervalId = setInterval(() => {
-        if (currentIndex < fullName.length) {
-          nameLink.textContent += fullName[currentIndex];
-          currentIndex++;
-        } else {
-          clearInterval(intervalId);
-        }
-      }, 100); 
-    });
-  
-    nameLink.addEventListener('mouseleave', () => {
+          if (currentIndex < fullName.length) {
+              nameLink.textContent += fullName[currentIndex];
+              currentIndex++;
+          } else {
+              clearInterval(intervalId);
+          }
+      }, 100);
+  });
+
+  nameLink.addEventListener('mouseleave', () => {
       nameLink.textContent = 'GUI';
       currentIndex = 0;
       clearInterval(intervalId);
-    });
-  
-    // Cursor personalizado
-    const cursor = document.querySelector('.cursor');
-    const links = document.querySelectorAll('a');
-  
-    document.body.addEventListener('mousemove', onMouseMove);
-  
-    links.forEach(link => {
+  });
+
+  // Cursor personalizado
+  const cursor = document.querySelector('.cursor');
+  const links = document.querySelectorAll('a');
+
+  document.body.addEventListener('mousemove', onMouseMove);
+
+  links.forEach(link => {
       link.addEventListener('mouseenter', onMouseEnterLink);
       link.addEventListener('mouseleave', onMouseLeaveLink);
-    });
-  
-    function onMouseMove(e) {
-      const rect = cursor.getBoundingClientRect();
-      const cursorX = e.clientX - rect.width / 2; 
-      const cursorY = e.clientY - rect.height / 2; 
-      
-      if (e.target.closest('section')) {
-        cursor.style.display = 'block'; 
-        cursor.style.top = cursorY + 'px';
-        cursor.style.left = cursorX + 'px';
-      } else {
-        cursor.style.display = 'none'; 
-      }
-    }
-  
-    function onMouseEnterLink() {
-      cursor.classList.add('hovered');
-    }
-  
-    function onMouseLeaveLink() {
-      cursor.classList.remove('hovered');
-    }
-  
-    // Efeito de imagem no projeto
-    const images = document.querySelectorAll('.image-list img');
-    const titles = document.querySelectorAll('.title-list h3');
-  
-    titles.forEach((title, index) => {
-      title.addEventListener('mouseenter', () => {
-        images.forEach(img => img.style.opacity = 0);
-  
-        const dataIndex = parseInt(title.getAttribute('data-index'));
-  
-        images[dataIndex].style.opacity = 1;
-      });
-  
-      title.addEventListener('mouseleave', () => {
-        images.forEach(img => img.style.opacity = 0);
-      });
-    });
   });
-  
+
+  function onMouseMove(e) {
+      const rect = cursor.getBoundingClientRect();
+      const cursorX = e.clientX - rect.width / 2;
+      const cursorY = e.clientY - rect.height / 2;
+
+      if (e.target.closest('section')) {
+          cursor.style.display = 'block';
+          cursor.style.top = cursorY + 'px';
+          cursor.style.left = cursorX + 'px';
+      } else {
+          cursor.style.display = 'none';
+      }
+  }
+
+  function onMouseEnterLink() {
+      cursor.classList.add('hovered');
+  }
+
+  function onMouseLeaveLink() {
+      cursor.classList.remove('hovered');
+  }
+
+  // Efeito de imagem no projeto
+  const images = document.querySelectorAll('.image-list img');
+  const titles = document.querySelectorAll('.title-list h3');
+  const popup = document.getElementById('popup');
+  const closeBtn = document.querySelector('.close');
+
+  titles.forEach((title, index) => {
+      title.addEventListener('mouseenter', () => {
+          images.forEach(img => img.style.opacity = 0);
+
+          const dataIndex = parseInt(title.getAttribute('data-index'));
+          images[dataIndex].style.opacity = 1;
+      });
+
+      title.addEventListener('mouseleave', () => {
+          images.forEach(img => img.style.opacity = 0);
+      });
+
+      title.addEventListener('click', () => {
+          const projectID = title.getAttribute('data-project');
+          const projectDetails = document.getElementById(projectID);
+
+          // Esconder todos os detalhes do projeto
+          document.querySelectorAll('.project-details').forEach(detail => {
+              detail.style.display = 'none';
+          });
+
+          // Mostrar os detalhes do projeto clicado
+          projectDetails.style.display = 'block';
+
+          popup.style.display = 'block';
+      });
+  });
+
+  // Fechar o popup quando o usuário clicar no <span> (x)
+  closeBtn.addEventListener('click', () => {
+      popup.style.display = 'none';
+  });
+
+  // Fechar o popup quando o usuário clicar fora do popup
+  window.addEventListener('click', (event) => {
+      if (event.target === popup) {
+          popup.style.display = 'none';
+      }
+  });
+}); // Chave que fecha a função DOMContentLoaded
